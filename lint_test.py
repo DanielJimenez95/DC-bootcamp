3fee636f38554f0131be7314c7e1317a407e8d04
Daniel <daniel@kalmy.mx>
2025-02-16 22:06:41 -0600

    exercises Sprint 7

---
+++ lint_test.py
@@ -0,0 +1,26 @@
+import math
+
+def is_prime(number):
+    """
+    Identifica si number es primo 
+    
+    """
+
+    if number <= 1:
+        return False
+    for i in range(2,int(math.sqrt(number) + 1)):
+        if number % i == 0:
+            return False
+    return True
+
+def main():
+
+    """tiene toda la lógica principal"""
+
+    for i in range(100):
+        if is_prime(i):
+            print (i, end=' ')
+    print()
+
+if __name__ == '__main__':
+    main()
